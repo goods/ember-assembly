dc28385582961e83f9f5123da2a012b77a2abb9b
Kyle Nathan <kyle@wearelion.com>
2018-12-28 12:21:20 +0000

    Add UiCombobox component

---
+++ addon/components/ui-combobox/component.ts
@@ -0,0 +1,65 @@
+import Component from "@ember/component";
+//@ts-ignore
+import template from "./template";
+import { layout } from "@ember-decorators/component";
+import { localClassNames } from "ember-css-modules";
+import { computed } from "@ember-decorators/object";
+import { isEmpty, isNone } from "@ember/utils";
+import { get } from "@ember/object";
+
+@localClassNames("ui-combobox")
+@layout(template)
+export default class UiCombobox extends Component {
+  selected!: any;
+  onSelect!: Function;
+  onDeselect!: Function;
+  labelPath?: string = "";
+  isLoading?: boolean = false;
+  isDisabled?: boolean = false;
+  options?: Array<any> = [];
+  placeholder?: string = "Please choose";
+  triggerComponent?: any = null;
+  optionComponent?: any = null;
+  hasTitle?: boolean = true;
+  hasFilter?: boolean = true;
+  title?: string = "Select options";
+  filter?: string = "";
+
+  @computed("selected.[]", "labelPath", "placeholder")
+  get triggerLabel(): string {
+    if (isEmpty(this.selected) && !isNone(this.placeholder)) {
+      return this.placeholder;
+    }
+
+    let selectedCount = get(this.selected, "length");
+    if (selectedCount > 1) {
+      return `${selectedCount} selected...`;
+    }
+    let value = get(this.selected, "firstObject");
+    if (!isNone(this.labelPath)) {
+      return get(value, this.labelPath);
+    }
+
+    return value;
+  }
+
+  @computed("options.[]", "filter")
+  get filteredOptions(): Array<any> {
+    if (isNone(this.options)) {
+      return [];
+    }
+
+    if (isNone(this.filter)) {
+      return this.options;
+    }
+
+    return this.options.filter(option => {
+      let label = option;
+      if (!isNone(this.labelPath)) {
+        label = get(option, this.labelPath);
+      }
+      //@ts-ignore
+      return label.toLowerCase().includes(this.filter.toLowerCase());
+    });
+  }
+}
