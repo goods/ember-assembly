64b442d6fdd48e87373a497136d0c85b28022f6a
Kyle Nathan <kyle@wearelion.com>
2019-01-02 13:39:49 +0000

    Add UiCheckbox

---
+++ addon/components/ui-checkbox/component.ts
@@ -0,0 +1,23 @@
+import Component from "@ember/component";
+// @ts-ignore: Ignore import of compiled template
+import template from "./template";
+import { localClassNames, localClassName } from "ember-css-modules";
+import { layout } from "@ember-decorators/component";
+import { isNone } from "@ember/utils";
+
+@localClassNames("ui-checkbox")
+@layout(template)
+export default class UiCheckbox extends Component {
+  label?: string = "";
+
+  @localClassName("is-checked")
+  checked?: boolean = false;
+  onChange?: Function | null = null;
+
+  click() {
+    if (!isNone(this.onChange)) {
+      this.onChange(!this.checked);
+    }
+    return false;
+  }
+}
