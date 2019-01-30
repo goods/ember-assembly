07f7ffdd9f9120637793ca687ea16cd72667ea70
Kyle Nathan <kyle@wearelion.com>
2019-01-30 21:18:04 +0000

    Expose ui-table class

--- addon/components/ui-table/component.ts
+++ addon/components/ui-table/component.ts
@@ -1,11 +1,12 @@
 import Component from "@ember/component";
 // @ts-ignore: Ignore import of compiled template
 import template from "./template";
-import { layout } from "@ember-decorators/component";
+import { layout, classNames } from "@ember-decorators/component";
 import { localClassNames, localClassName } from "ember-css-modules";
 import { notEmpty } from "@ember-decorators/object/computed";
 import { computed } from "@ember-decorators/object";
 
+@classNames("ui-table")
 @localClassNames("ui-table")
 @layout(template)
 export default class UiTable extends Component {
