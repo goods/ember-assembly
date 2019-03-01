6726f28dd28a2770c531d09e41112045902405f8
Kyle Nathan <kyle@wearelion.com>
2019-03-01 09:03:47 +0000

    Make layout primitive values configurable

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ typography.ui = `"SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Ro
 typography.mono = `"SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace`;
 
 let layout = {};
+layout["scale-1"] = "2px";
+layout["scale-2"] = "4px";
+layout["scale-3"] = "8px";
+layout["scale-4"] = "16px";
+layout["scale-5"] = "32px";
+layout["scale-6"] = "64px";
+layout["scale-7"] = "128px";
+layout["scale-8"] = "256px";
+layout["scale-9"] = "512px";
+layout["scale-10"] = "1024px";
+
 module.exports = {
   name: require("./package").name,
 
