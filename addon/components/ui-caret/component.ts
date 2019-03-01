77d1efcc164fadcec179307f7cd2a7a4112869ed
Kyle Nathan <kyle@wearelion.com>
2019-03-01 08:51:27 +0000

    Export a class name for UiCaret

--- addon/components/ui-caret/component.ts
+++ addon/components/ui-caret/component.ts
@@ -2,8 +2,9 @@ import Component from "@ember/component";
 // @ts-ignore: Ignore import of compiled template
 import template from "./template";
 import { localClassNames, localClassName } from "ember-css-modules";
-import { layout } from "@ember-decorators/component";
+import { layout, classNames } from "@ember-decorators/component";
 
+@classNames("ui-caret")
 @localClassNames("ui-caret")
 @layout(template)
 export default class UiCaret extends Component {
