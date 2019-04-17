ed1bc565c02ecad3688b9f46637376d74d450e72
Kyle Nathan <kyle@wearelion.com>
2019-04-17 16:59:29 +0100

    Refactor UiTextarea

--- addon/components/ui-textarea/component.ts
+++ addon/components/ui-textarea/component.ts
@@ -1,32 +1,30 @@
 import Component from "@ember/component";
 // @ts-ignore: Ignore import of compiled template
 import template from "./template";
-import { layout, tagName, classNames } from "@ember-decorators/component";
+import { layout, classNames } from "@ember-decorators/component";
 import { localClassNames, localClassName } from "ember-css-modules";
 import { isNone } from "@ember/utils";
 import { set } from "@ember/object";
+import { action } from "@ember-decorators/object";
 
 @classNames("ui-textarea")
 @localClassNames("ui-textarea")
 @layout(template)
-@tagName("textarea")
 export default class UiTextarea extends Component {
   id?: string = this.elementId;
   value?: string = "";
-  onSetValue?: Function | null = null;
+  onChange?: Function | null = null;
   required?: boolean = false;
   disabled?: boolean = false;
 
   @localClassName()
   hasError?: boolean = false;
 
-  keyUp(event: KeyboardEvent) {
-    //@ts-ignore
-    let value = event.currentTarget.value;
-
-    if (isNone(this.onSetValue)) {
+  @action
+  onKeyUp(value: string) {
+    if (isNone(this.onChange)) {
       return set(this, "value", value);
     }
-    return this.onSetValue(value, event);
+    return this.onChange(value, event);
   }
 }
