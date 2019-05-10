ef2c79a6db06e992818488068df8dfbedf8ffdb9
Kyle Nathan <kyle@wearelion.com>
2019-05-10 11:48:54 +0100

    Add UiTag component

---
+++ addon/components/ui-tag/component.ts
@@ -0,0 +1,29 @@
+import Component from "@ember/component";
+// @ts-ignore: Ignore import of compiled template
+import template from "./template";
+import { localClassNames, localClassName } from "ember-css-modules";
+import { layout, tagName } from "@ember-decorators/component";
+import { isNone } from "@ember/utils";
+import { computed } from "@ember-decorators/object";
+
+@localClassNames("ui-tag")
+@layout(template)
+@tagName("button")
+export default class UiTag extends Component {
+  label!: string;
+  icon?: string | null = null;
+  appearance?: string = "default"; //Enum: default, strong, minimal
+  onClick?: Function | null = null;
+
+  @localClassName()
+  @computed("appearance")
+  get appearanceClass(): string {
+    return `appearance-${this.appearance}`;
+  }
+
+  click() {
+    if (!isNone(this.onClick)) {
+      this.onClick();
+    }
+  }
+}
