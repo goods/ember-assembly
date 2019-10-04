7aed96b023092802eac7d6054313363e7277b3c1
Kyle Nathan <kyle@wearelion.com>
2019-10-04 10:28:27 +0100

    Fix UiSearchInput

--- addon/components/ui-search-input/component.ts
+++ addon/components/ui-search-input/component.ts
@@ -3,9 +3,21 @@ import Component from "ember-assembly/components/ui-text-input/component";
 import template from "./template";
 import { localClassNames } from "ember-css-modules";
 import { layout } from "@ember-decorators/component";
+import { action, set } from "@ember/object";
+import { isNone } from "@ember/utils";
 
 @localClassNames("ui-search-input")
 @layout(template)
 export default class UiSearchInput extends Component {
+  value?: string = "";
+  onChange?: Function | null = null;
   isSearching?: boolean = false;
+
+  @action
+  onKeyUp(value: string) {
+    if (isNone(this.onChange)) {
+      return set(this, "value", value);
+    }
+    return this.onChange(value, event);
+  }
 }
