9ddb28864982791b2e3a647bd702025dee697283
Kyle Nathan <kyle@wearelion.com>
2020-03-23 21:55:47 +0000

    Add notifications

---
+++ addon/components/ui-notification/component.ts
@@ -0,0 +1,72 @@
+import Component from "@ember/component";
+import { get, set, action } from "@ember/object";
+import { notEmpty } from "@ember/object/computed";
+// @ts-ignore: Ignore import of compiled template
+import template from "./template";
+import { layout, classNames } from "@ember-decorators/component";
+import { localClassNames, localClassName } from "ember-css-modules";
+import { timeout } from "ember-concurrency";
+import { task } from "ember-concurrency-decorators";
+import { computed } from "@ember/object";
+import { htmlSafe } from "@ember/string";
+
+@classNames("ui-notification")
+@localClassNames("ui-notification")
+@layout(template)
+export default class UiNotification extends Component {
+  notification!: any;
+  onDismiss!: Function;
+
+  interval?: number = 20; //ms
+
+  progress: number = 0;
+
+  @localClassName()
+  @computed("notification.intent")
+  get intentClass(): string {
+    return `intent-${this.notification.intent}`;
+  }
+
+  @localClassName()
+  @notEmpty("notification.description")
+  hasDescription!: boolean;
+
+  @computed("notification.description")
+  get safeDescription() {
+    return htmlSafe(get(this.notification, "description"));
+  }
+
+  @task
+  progressTask = function*(
+    this: UiNotification,
+    notification: any,
+    intervalMs: number
+  ) {
+    while (this.progress < 100) {
+      yield timeout(intervalMs);
+      let progressTotal =
+        (this.progress * notification.timeout) / 100 + intervalMs;
+      let progress = (progressTotal / notification.timeout) * 100;
+      set(this, "progress", progress);
+    }
+    this.onDismiss();
+  };
+
+  @action
+  cancelProgress() {
+    //@ts-ignore
+    this.progressTask.cancelAll();
+  }
+
+  @action
+  resumeProgress() {
+    //@ts-ignore
+    this.progressTask.perform(this.notification, this.interval);
+  }
+
+  init() {
+    super.init();
+    //@ts-ignore
+    this.progressTask.perform(this.notification, this.interval);
+  }
+}
---
+++ addon/services/notifications.ts
@@ -0,0 +1,42 @@
+import Service from "@ember/service";
+
+interface NotificationParams {
+  title: string;
+  description: string;
+  intent: "info" | "success" | "warning" | "danger";
+  timeout: number;
+}
+
+export default class Notifications extends Service {
+  notifications: any = [];
+
+  nextId: number = 0;
+
+  createNotification({
+    title,
+    description = "",
+    intent = "info",
+    timeout = 5000
+  }: NotificationParams) {
+    let id = this.get("nextId");
+    this.notifications.pushObject({
+      id,
+      title: title,
+      description: description,
+      intent: intent,
+      timeout: timeout
+    });
+
+    this.incrementProperty("nextId");
+  }
+
+  destroyNotification(notification: any) {
+    this.notifications.removeObject(notification);
+  }
+}
+
+declare module "@ember/service" {
+  interface Registry {
+    notifications: Notifications;
+  }
+}
