acd7bc04059ca403a092931db0604da963503890
Kyle Nathan <kyle@wearelion.com>
2020-03-23 21:54:48 +0000

    Add base-font-size and small-font-size vars

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let typography = {};
 typography.display = `"SF UI Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
 typography.ui = `"SF UI Text", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
 typography.mono = `"SF Mono", "Monaco", "Inconsolata", "Fira Mono", "Droid Sans Mono", "Source Code Pro", monospace`;
+typography["base-font-size"] = "13px";
+typography["small-font-size"] = "12px";
 
 let layout = {};
 layout["scale-1"] = "2px";
