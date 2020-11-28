f40fc0ce5d48b1d341f11d0cf21768249594037a
Kyle Nathan <kyle@wearelion.com>
2020-11-28 10:18:39 +0000

    Refactor UiPopover to work with lates EmberBasicDropdown

--- addon/components/ui-popover/component.ts
+++ addon/components/ui-popover/component.ts
@@ -1,19 +1,21 @@
+//@ts-ignore see: https://github.com/cibernox/ember-basic-dropdown/issues/542
+import BasicDropdown from "ember-basic-dropdown/components/basic-dropdown";
 //@ts-ignore
-import Component from "ember-basic-dropdown/components/basic-dropdown";
+import { setComponentTemplate } from "@ember/component";
 //@ts-ignore
-import template from "ember-basic-dropdown/templates/components/basic-dropdown";
+import layout from "ember-basic-dropdown/templates/components/basic-dropdown";
 import { localClassNames } from "ember-css-modules";
 
-/*
-  Must add ui-popover-content class to yielded Popover.content.
+// /*
+//   Must add ui-popover-content class to yielded Popover.Content.
 
-  Add the class ui-popover-animated to yielded Popover.content to animate.
-  <Popover.content @class="ui-popover-content ui-popover-animated">
-*/
+//   Add the class ui-popover-animated to yielded Popover.Content to animate.
+//   <Popover.Content class="ui-popover-content ui-popover-animated">
+// */
 
 @localClassNames("ui-popover")
-export default class UiPopover extends Component {
-  layout = template;
-
+class UiPopover extends BasicDropdown {
   useAnimation?: boolean = true;
 }
+
+export default setComponentTemplate(layout, UiPopover);
