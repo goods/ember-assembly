031c542fec34fd47582d20f45ae4dbd4b98b2034
Kyle Nathan <kyle@wearelion.com>
2020-12-09 17:27:33 +0000

    Change UiSelectOption to use buttons so that we can click in tests

--- addon/components/ui-select-option/component.ts
+++ addon/components/ui-select-option/component.ts
@@ -2,14 +2,20 @@ import Component from "@ember/component";
 // @ts-ignore: Ignore import of compiled template
 import template from "./template";
 import { localClassNames } from "ember-css-modules";
+import { tagName, attribute, classNames } from "@ember-decorators/component";
 
+@classNames("ui-select-option")
 @localClassNames("ui-select-option")
+@tagName("button")
 export default class UiSelectOption extends Component {
   layout = template;
 
   onChange!: Function;
   onClose!: Function;
+
+  @attribute("data-value")
   value!: any;
+
   label?: string = "";
   labelPath?: string = "";
   optionComponent?: any = null;
