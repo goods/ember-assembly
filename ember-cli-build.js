350e0dfbeab42e1ffc144bb2957e5f5b670f638f
Kyle Nathan <kyle@wearelion.com>
2021-07-22 15:20:19 +0100

    Use CSS variables

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,15 +2,15 @@
 
 const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
 
-module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {});
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
+module.exports = function (defaults) {
+  let app = new EmberAddon(defaults, {
+    cssModules: {
+      headerModules: [
+        "ember-assembly/styles/palette",
+        "ember-assembly/styles/typography",
+      ],
+    },
+  });
 
   return app.toTree();
 };
