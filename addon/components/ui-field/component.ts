ddb79da3968718f7d979e97765042205b8409036
Kyle Nathan <kyle@wearelion.com>
2022-01-17 14:41:35 +0000

    Dont require label in UiField

--- addon/components/ui-field/component.ts
+++ addon/components/ui-field/component.ts
@@ -2,7 +2,6 @@ import Component from "@ember/component";
 // @ts-ignore: Ignore import of compiled template
 import template from "./template";
 import { localClassNames, localClassName } from "ember-css-modules";
-import { assert } from "@ember/debug";
 import { isNone } from "@ember/utils";
 import { classNames } from "@ember-decorators/component";
 import { computed } from "@ember/object";
@@ -12,9 +11,20 @@ import { computed } from "@ember/object";
 export default class UiField extends Component {
   layout = template;
 
-  label!: string;
+  label?: string | null = null;
   description?: string = "";
-  validationMessages?: Array<string> = [];
+  validationMessages?: string[] | string = [];
+
+  @computed("validationMessages")
+  get errors(): string[] | null {
+    if (isNone(this.validationMessages)) {
+      return null;
+    }
+    if (Array.isArray(this.validationMessages) == false) {
+      return [this.validationMessages];
+    }
+    return this.validationMessages;
+  }
 
   @localClassName()
   direction?: string = "vertical"; //vertical | horizontal
@@ -22,10 +32,5 @@ export default class UiField extends Component {
   @computed("elementId")
   get inputId(): string {
     return `${this.elementId}-input`;
-  }
-
-  init() {
-    super.init();
-    assert("`label` is required", !isNone(this.label));
   }
 }
