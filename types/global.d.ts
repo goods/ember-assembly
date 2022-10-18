b5296894b0d3119b620c2800ccbe7f3e9dc1ef76
Kyle Nathan <kyle@wearelion.com>
2022-10-18 10:47:54 +0100

    Update ember-cli-typescript

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,7 @@
 // Types for compiled templates
-declare module 'ember-assembly/templates/*' { 
-  import { TemplateFactory } from 'htmlbars-inline-precompile';
+declare module 'ember-assembly/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
+
   const tmpl: TemplateFactory;
   export default tmpl;
 }
